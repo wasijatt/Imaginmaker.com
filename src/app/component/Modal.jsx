4d9537bfae2a6f5799ad03e754ee27a2e816e7ce
MUHAMMAD <muhmmadwasim5686@gmail.com>
2024-05-02 06:21:46 -0700

    erorr handlings .

--- src/app/component/Modal.jsx
+++ src/app/component/Modal.jsx
@@ -1,4 +1,4 @@
-
+"use client"
 import { motion } from 'framer-motion';
 import { MdOutlineNavigateBefore, MdOutlineNavigateNext } from "react-icons/md";
 import Image from 'next/image';
--- src/app/component/OurServices.jsx
+++ src/app/component/OurServices.jsx
@@ -1,3 +1,4 @@
+"use client"
 import style from "../modulerCss/HeroSection.module.css";
 
 import Image from "next/image";
