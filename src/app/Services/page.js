6cd4587ec2c0b150e7875b215362a2a0a4336815
MUHAMMAD <muhmmadwasim5686@gmail.com>
2024-05-28 12:12:46 -0700

    fully reposiveness of front page

--- src/app/Services/page.js
+++ src/app/Services/page.js
@@ -1,4 +1,5 @@
 import Loading from "../component/Loading";
+
 import dynamic from "next/dynamic";
 const ServicesPage = dynamic(() => import("./../component/ServicesPage/ServicesPage"), {
   ssr: true,
