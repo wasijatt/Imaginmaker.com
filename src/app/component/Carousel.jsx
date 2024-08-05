faf0b07ac1e8a24282c538a7878a92b8bd9b4c92
Muhammad Wasim <muhammmadwasim5686@gmail.com>
2024-08-05 10:41:18 -0700

    testing first compoen

--- src/app/component/Carousel.jsx
+++ src/app/component/Carousel.jsx
@@ -20,10 +20,10 @@ const Carousel = () => {
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5 }}
             key={index}
-            className="mxl:w-[350px] 3xl:w-[400px] lg:px-7 p-1"
+            className="mxl:w-[350px] 3xl:w-[400px] lg:px-5 p-1"
           >
             <Image
-              className="rounded-2xl"
+              className="cariborder"
               src={src}
               width={400}
               height={400}
