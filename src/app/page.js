fb2f333a08e55f2aec4d5e40326f7885d1ce3241
Muhammad Wasim <muhammmadwasim5686@gmail.com>
2024-10-29 01:33:50 -0700

    fixing heat memory error

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,18 @@
 "use client"
-import { useEffect, useState } from "react";
 
 import dynamic from "next/dynamic";
 
 const HomePage = dynamic(() => import("./component/mainPage/HomePage"), {
-  ssr: true,
-  loading: () => <div className="bg-black animate-pulse h-32 w-full"><Loading/></div>,
-});
-const Loading = dynamic(() => import("./component/Loading"), {
   ssr: false,
-  loading: () => <div className="bg-black animate-pulse h-32 w-full"><Loading/></div>,
+  loading: () => <div className="bg-black animate-pulse h-32 w-full"></div>,
 });
 
 export default function Page() {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Simulate a loading period before rendering the HomePage
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000); // Adjust the time (e.g., 2 seconds) for how long the loading should last
-
-    return () => clearTimeout(timer); // Cleanup the timeout
-  }, []);
 
   return (
     <>
-      {loading ? (
-        
-        <div className="">
-          <Loading />
-        </div>
-      ) : (
-       
-        <HomePage />
-      )}
+     <HomePage/>
     </>
   );
 }
