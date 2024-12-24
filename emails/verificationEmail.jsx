e88153bae98c8ff460ce4e16292e243460d2d1b2
Muhammad Wasim <115286199+wasijatt@users.noreply.github.com>
2024-12-24 09:29:04 -0800

    Delete emails directory

--- emails/verificationEmail.jsx
+++
@@ -1,8 +0,0 @@
-const EmailTemplate = ({ firstName }) => `
-    <div>
-        <h1>Welcome, ${firstName}!</h1>
-        <p>We’re excited to have you on board. Let us know if you have any questions.</p>
-    </div>
-`;
-
-export default EmailTemplate;
