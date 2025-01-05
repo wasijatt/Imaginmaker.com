120fefa7222adc415c36fe3a4c9df65475e65632
ZohaRehman <jk03075837@gmail.com>
2025-01-05 12:08:56 +0500

    services page mistake sets

--- src/components/services/firstSection.js
+++ src/components/services/firstSection.js
@@ -25,7 +25,7 @@ export default function ServicesFirstSection() {
                         <br />
                         <span className="flex items-center">
                             for Achieving Goals
-                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='relative left-[0.3rem] max-sm:left-[-12.8rem] max-sm:top-6' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g filter="url(#filter0_i_3597_81)">
                                     <circle cx="7.5" cy="7.5" r="7.5" fill="url(#paint0_linear_3597_81)" />
                                 </g>
