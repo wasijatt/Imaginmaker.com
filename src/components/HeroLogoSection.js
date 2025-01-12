11f82a342da2ef021367116337e61734cb0206bf
ZohaRehman <jk03075837@gmail.com>
2025-01-12 15:01:59 +0500

    logo marquee

--- src/components/HeroLogoSection.js
+++ src/components/HeroLogoSection.js
@@ -9,97 +9,97 @@ export function LogoMarquee() {
             <div className="relative flex gap-0">
                 {/* First set of logos */}
                 <div className="flex animate-marquee whitespace-nowrap gap-0">
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-3 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo1.png"
                             alt="Company Logo 1"
                             width={250}
                             height={100}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo2.png"
                             alt="Company Logo 2"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo3.png"
                             alt="Company Logo 3"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo4.png"
                             alt="Company Logo 4"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo5.png"
                             alt="Company Logo 5"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
                 </div>
                 {/* Second set of logos (duplicate for seamless loop) */}
                 <div className="flex animate-marquee2 whitespace-nowrap gap-0 absolute left-0">
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo1.png"
                             alt="Company Logo 1"
-                            width={200}
-                            height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            width={250}
+                            height={100}
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo2.png"
                             alt="Company Logo 2"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo3.png"
                             alt="Company Logo 3"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo4.png"
                             alt="Company Logo 4"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
-                    <div className="mx-8 sm:mx-12 md:mx-16 lg:mx-20 flex items-center">
+                    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 flex items-center">
                         <Image
                             src="/logoanimation/animationlogo5.png"
                             alt="Company Logo 5"
                             width={200}
                             height={70}
-                            className="max-h-8 sm:max-h-10 md:max-h-12 w-auto"
+                            className="max-h-12 sm:max-h-12 md:max-h-16 w-auto"
                         />
                     </div>
                 </div>
@@ -107,4 +107,3 @@ export function LogoMarquee() {
         </div>
     );
 }
-
