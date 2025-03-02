07241d4d290f9d705c150e7604d0c470862e2051
ZohaRehman <jk03075837@gmail.com>
2025-03-02 19:30:10 +0500

    site icon changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/siteicon.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/siteicon.svg" />
         <link rel="icon" type="image/svg" sizes="32x32" href="/siteicon-32x32.svg" />
         <link rel="icon" type="image/svg" sizes="16x16" href="/siteicon.svg" />
