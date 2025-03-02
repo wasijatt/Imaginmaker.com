bfb8f70e9f73ffbe81feea7448be26e43ce39de9
ZohaRehman <jk03075837@gmail.com>
2025-03-02 19:13:04 +0500

    delete blog

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Add meta tags, fonts, or external stylesheets here if needed */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/siteicon.svg" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/siteicon-32x32.svg" />
+        <link rel="icon" type="image/svg" sizes="16x16" href="/siteicon.svg" />
       </Head>
       <body>
         <Main />
